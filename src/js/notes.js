8eab24008450a4aec03fe164f1a0e00a46b92da3
ooorange <77827963+ooorange777@users.noreply.github.com>
2024-06-11 17:00:12 +0000

    调整样式

--- src/js/notes.js
+++ src/js/notes.js
@@ -1,22 +1,35 @@
 let dataAcct = "juju@f.jymuoyu.com";
 let dataLimit = 10;
-const url = "https://notestock.osa-p.net/api/v1/search.json?acct=" + dataAcct + "&limit=" + dataLimit;
-const widget = document.querySelector('#ns-container');
-fetch(url).then((response) => {
-  return response.json();
-}).then((data) => {
-  data.statuses.forEach((status) => {
-    let statusframe = document.createElement("div");
-    let title = document.createElement("p");
-    widget.appendChild(statusframe);
-    statusframe.appendChild(title);
-    title.textContent = "ooorange 说：";
-    let content = document.createElement("p");
-    statusframe.appendChild(content);
-    content.textContent = status.notag;
-    let published = new Date(status.published);
-    let time = document.createElement("p");
-    statusframe.appendChild(time);
-    time.textContent = published.toLocaleString();
+const url =
+  "https://notestock.osa-p.net/api/v1/search.json?acct=" +
+  dataAcct +
+  "&limit=" +
+  dataLimit;
+
+fetch(url)
+  .then((response) => {
+    return response.json();
   })
-})
+  .then((data) => {
+    data.statuses.forEach((status) => {
+      const widget = document.querySelector("#ns-container");
+      let statusframe = document.createElement("div");
+      let title = document.createElement("p");
+      widget.appendChild(statusframe);
+      statusframe.appendChild(title);
+      statusframe.className = "mt-2";
+      statusframe.classList.add("mb-5");
+      title.textContent = "ooorange 说：";
+      title.className = "mb-2";
+      let content = document.createElement("p");
+      statusframe.appendChild(content);
+      content.textContent = status.notag;
+      let published = new Date(status.published);
+      let time = document.createElement("p");
+      time.className = "text-xs";
+      time.classList.add("pt-2");
+      time.classList.add("text-end");
+      statusframe.appendChild(time);
+      time.textContent = published.toLocaleString();
+    });
+  });
